02216de8310103d362ada7a75357ae650054a3bb
ProtieusKeebs <118025702+protieusz@users.noreply.github.com>
2023-11-05 23:21:02 -0800

    Update config.h to include 8 RGBs

--- dragonfruit02/config.h
+++ dragonfruit02/config.h
@@ -57,7 +57,7 @@
 #define WS2812_DI_PIN GP29
 
 #ifdef RGBLIGHT_ENABLE
-    #define RGBLED_NUM 3
+    #define RGBLED_NUM 8
     #define RGBLIGHT_LIMIT_VAL 80
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
