3b1d3672c1add3d8c99f7e78fa99d380d4c98021
ProtieusKeebs <118025702+protieusz@users.noreply.github.com>
2023-11-07 00:25:51 -0800

    Updated code to add scroll lock keycode

--- dragonfruit02/keymaps/default/keymap.c
+++ dragonfruit02/keymaps/default/keymap.c
@@ -20,16 +20,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum combo_events {
   COMBO_DEL,
   COMBO_ESC,
+  COMBO_RCLICK,
   COMBO_LENGTH
 };
 uint16_t COMBO_LEN = COMBO_LENGTH;
 
 const uint16_t PROGMEM combo_del[] = {KC_Y, KC_H, COMBO_END};
 const uint16_t PROGMEM combo_esc[] = {KC_T, KC_G, COMBO_END};
+const uint16_t PROGMEM combo_rclick[] = {KC_MS_BTN1, SCRL_MO, COMBO_END};
 
 combo_t key_combos[] = {
   [COMBO_DEL] = COMBO(combo_del, KC_DEL),
   [COMBO_ESC] = COMBO(combo_esc, KC_ESC),
+  [COMBO_RCLICK] = COMBO(combo_rclick, KC_MS_BTN2),
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -49,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,         KC_W, KC_E,    KC_R,    KC_T,                                           KC_Y,          KC_U,          KC_I,     KC_O,   KC_P,
         KC_A,         KC_S, KC_D,    KC_F,    KC_G,                                           KC_H,          KC_J,          KC_K,     KC_L,   KC_SCLN,
         LSFT_T(KC_Z), KC_X, KC_C,    KC_V,    KC_B,                                           KC_N,          KC_M,          KC_COMMA, KC_DOT, RSFT_T(KC_SLSH),
-                            KC_LCTL, KC_LGUI, LT(1,KC_BSPC),   KC_MS_BTN2,    KC_MS_BTN1,     LT(2,KC_SPC),   (KC_ENT),      KC_LALT
+                            KC_LCTL, KC_LGUI, LT(1,KC_BSPC),   KC_MS_BTN1,    SCRL_MO,     LT(2,KC_SPC),   (KC_ENT),      KC_LALT
     ),
     [_FN1] = LAYOUT(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,     KC_7,    KC_8,    KC_9,    KC_0,
@@ -60,13 +63,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN2] = LAYOUT(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
         RGB_VAI,  RGB_SAI,    RGB_HUI,    RGB_SPI,   RGB_MOD,                RGB_TOG,  _______,  _______,   _______, _______,
-        RGB_VAD,  RGB_SAD,    RGB_HUD,    RGB_SPD,   RGB_RMOD,               KC_ACL0,  KC_ACL1, KC_ACL2, _______, _______,
+        RGB_VAD,  RGB_SAD,    RGB_HUD,    RGB_SPD,   RGB_RMOD,               _______,  _______, _______, _______, _______,
                           _______, _______, _______,     _______, _______,   _______,  _______, _______
     ),
     [_FN3] = LAYOUT(
         _______, KC_UP, _______, _______, _______,                         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_PSCR,
         KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______,                      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,
-        _______, _______, _______, _______, _______,                       KC_F10,  KC_F11,  KC_F12,  _______, _______,
+        _______, _______, ROT_L15, ROT_R15, CPI_SW,                       KC_F10,  KC_F11,  KC_F12,  _______, _______,
                           _______, _______, _______,     _______, _______,   _______, _______, _______
     )
 };
